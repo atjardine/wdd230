c1b502e8c2e0be5d74d8b7a24bfe1e00a1fe2733
Use your GitHub username <Enter your email address>
2022-01-05 20:54:43 -0600

    date

--- js/shortdatecurrent.js
+++ js/shortdatecurrent.js
@@ -1,10 +1,1 @@
-const options = {
-    day: "numeric",
-    month: "numberic",
-    year: "numeric",
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric"
-  };
-  document.getElementById("shortdatecurrent").textContent =
-    new Date().toLocaleDateString("en-US", options);
+new Date().toLocaleString('en-US')
