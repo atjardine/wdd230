dbfa858284c2f06ed355c1aeb366647b0f1e4cd2
Use your GitHub username <Enter your email address>
2022-01-05 20:01:09 -0600

    practiceindex

--- js/currentdate.js
+++ js/currentdate.js
@@ -1,2 +1,3 @@
 const options = {weekday: 'long', day: 'numeric', month: 'long', year: 'numeric'};
 document.getElementById('currentdate').textContent = new Date().toLocaleDateString('en-US', options);
+`const daysUntil = "There are" ${daycount} " days until " ${targetEvent}"!"`
